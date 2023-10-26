038d75bd08124879527c173acbe814e0ae9584ef
00wz <130168402+00wz@users.noreply.github.com>
2023-10-26 20:41:34 +0300

    PlayerCharacter implements ISavable

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,7 +1,22 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour, ICharacter
+public class PlayerCharacter : MonoBehaviour, ICharacter,ISaveble
 {
     public IDictionary<Currency, int> inventary => GameRootInstance.Instance.MainInventory.Inventory;
+
+    public void Load(ArrayList saveParam)
+    {
+        transform.position = new Vector3((float)saveParam[0], (float)saveParam[1], (float)saveParam[2]);
+    }
+
+    public ArrayList Save()
+    {
+        ArrayList saveParam = new();
+        saveParam.Add(transform.position.x);
+        saveParam.Add(transform.position.y);
+        saveParam.Add(transform.position.z);
+        return saveParam;
+    }
 }
